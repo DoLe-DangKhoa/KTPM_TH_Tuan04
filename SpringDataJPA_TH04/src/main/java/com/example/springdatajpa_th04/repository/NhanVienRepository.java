5b3b82e05c36b00943da0882dfe4890776b0e030
DoLe-DangKhoa <doledangkhoaa@gmail.com>
2022-10-08 11:29:13 +0700

    cau 3 +8 +9 +10 +12 +15 +22

--- SpringDataJPA_TH04/src/main/java/com/example/springdatajpa_th04/repository/NhanVienRepository.java
+++ SpringDataJPA_TH04/src/main/java/com/example/springdatajpa_th04/repository/NhanVienRepository.java
@@ -8,5 +8,33 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface NhanVienRepository extends JpaRepository<NhanVien, String> {
+    @Query("select nv from NhanVien nv where nv.luong < 10000")
+    List<NhanVien> findNhanVienByLuong();
+    @Query("select Sum(nv.luong) from NhanVien nv")
+    int tinhTongLuong();
 
+    @Query(value = "select nv.maNV from ChungNhan cn, MayBay mb, NhanVien nv " +
+            "where nv.maNV = cn.MaNV and mb.maMB = cn.MaMB and mb.loai like 'Boeing%'", nativeQuery = true)
+    List<String> getMaByLoaiMayBay();
+
+    @Query(value = "select nv.maNV, nv.ten, nv.luong from NhanVien nv, ChungNhan cn" +
+            " where nv.maNV = cn.MaNV and cn.MaMB = ?1", nativeQuery = true)
+    List<NhanVien> findNhanVienByMaMB(int maMB);
+
+    @Query(value = "select nv.maNV from NhanVien nv " +
+            "where nv.maNV in (select cn.MaNV from ChungNhan cn, MayBay mb " +
+            "where cn.MaMB = mb.maMB and mb.loai like 'Boeing%') " +
+            "and nv.maNV in (select cn.MaNV from ChungNhan cn, MayBay mb " +
+            "where cn.MaMB = mb.maMB and mb.loai like 'Airbus%')", nativeQuery = true)
+    List<String> getMaByLoaiMB();
+
+    @Query(value = "select nv.ten from NhanVien nv " +
+            "where nv.maNV in (select cn.MaNV from ChungNhan cn, MayBay mb " +
+            "where cn.MaMB = mb.maMB and mb.loai like 'Boeing%')", nativeQuery = true)
+    List<String> findTenNhanVienLaiBoeing();
+    @Query(value = "select nv.maNV from NhanVien nv " +
+            "where nv.maNV in (select cn.MaNV from ChungNhan cn " +
+            "group by cn.MaNV " +
+            "having count(cn.maMB) >= 3)", nativeQuery = true)
+    List<String> findMaPhiCongLai3LoaiMayBay();
 }
--- SpringDataJPA_TH04/src/test/java/com/example/springdatajpa_th04/repository/NhanVienRepositoryTests.java
+++ SpringDataJPA_TH04/src/test/java/com/example/springdatajpa_th04/repository/NhanVienRepositoryTests.java
@@ -6,6 +6,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class NhanVienRepositoryTests {
+    @Autowired
+    private NhanVienRepository nhanVienRepository;
+    @Test
+    void cau3(){
+        System.out.println(nhanVienRepository.findNhanVienByLuong());
+    }
+    @Test
+    void cau8(){
+        System.out.println("Tổng lương phải trả cho nhân viên: " + nhanVienRepository.tinhTongLuong());
+    }
+    @Test
+    void cau9(){
+        System.out.println(nhanVienRepository.getMaByLoaiMayBay());
+    }
+    @Test
+    void cau10(){
+        System.out.println(nhanVienRepository.findNhanVienByMaMB(747));
+    }
+    @Test
+    void cau12(){
+        System.out.println(nhanVienRepository.getMaByLoaiMB());
+    }
 
+    @Test
+    void cau15(){
+        System.out.println(nhanVienRepository.findTenNhanVienLaiBoeing());
+    }
+    @Test
+    void cau22(){
+        System.out.println(nhanVienRepository.findMaPhiCongLai3LoaiMayBay());
+    }
 
 }
